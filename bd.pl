61aebef607e649238e662122f66f7b75d2a01bed
Ania <jana.polya@yandex.ru>
2025-04-14 09:13:40 +0300

    number3 add grandparents

--- bd.pl
+++ bd.pl
@@ -8,15 +8,29 @@ woman(galya).
 woman(sveta).
 woman(zoya).
 woman(katrin).
-dite(dimitriy, anatoliy).
-dite(dimitriy, galya).
-dite(vladina, anatoliy).
-dite(vladina, galya).
-dite(kirill, dimitriy).
-dite(mefodiy, dimitriy).
-dite(kirill, sveta).
-dite(mefodiy, sveta).
-dite(zoya, vlad).
-dite(zoya, vladina).
-dite(katrin, vlad).
-dite(katrin, vladina).
+
+
+dite(ivan, oleg).      % Олег — отец Ивана 
+dite(ivan, lyuba).     % Люба — мать Ивана
+dite(galya, oleg).     % Олег — отец Гали
+dite(galya, lyuba).    % Люба — мать Гали
+dite(maria, oleg).     % Олег — отец Марии 
+dite(maria, olga).     % Ольга — мать Марии 
+
+
+dite(anatoliy, ivan).  % Иван — отец Анатолия
+dite(anatoliy, maria). % Мария — мать Анатолия
+dite(vladina, ivan).   % Иван — отец Владины
+dite(vladina, maria).  % Мария — мать Владины
+dite(dimitriy, anatoliy). % Анатолий — отец Дмитрия
+dite(dimitriy, galya).    % Галя — мать Дмитрия
+
+
+dite(kirill, dimitriy).  % Дмитрий — отец Кирилла
+dite(kirill, sveta).     % Света — мать Кирилла
+dite(mefodiy, dimitriy). % Дмитрий — отец Мефодия
+dite(mefodiy, sveta).    % Света — мать Мефодия
+dite(zoya, vlad).        % Владимир — отец Зои
+dite(zoya, vladina).     % Владина — мать Зои
+dite(katrin, vlad).      % Владимир — отец Катрин
+dite(katrin, vladina). 
